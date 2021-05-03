7954165317be3f98f60d11a6af1e0a66a18f6307
Elan-R <63056651+Elan-R@users.noreply.github.com>
2021-05-03 11:55:56 -0400

    Update winpy.py

--- Small Scripts/winpy.py
+++ Small Scripts/winpy.py
@@ -1,6 +1,6 @@
 from code import InteractiveConsole
 from os import system
-from sys import version
+from sys import version, exit
 
 console = InteractiveConsole()
 console.push("import os")
@@ -8,10 +8,13 @@ console.push("import os")
 print(version)
 print("Start a line with $ to be interpreted in the command line.")
 
-while True:
-    code = console.raw_input(">>> ")
-    if code.startswith("$"):
-        system(code[1:])
-        continue
-    while console.push(code):
-        code = console.raw_input("... ")
+try:
+    while True:
+        code = console.raw_input(">>> ")
+        if code.startswith("$"):
+            system(code[1:])
+            continue
+        while console.push(code):
+            code = console.raw_input("... ")
+finally:
+    exit()
