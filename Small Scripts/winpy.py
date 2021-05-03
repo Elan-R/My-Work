d0c8d2268664ec1168298d912a0d3d50d9cad38a
Elan-R <63056651+Elan-R@users.noreply.github.com>
2021-05-03 12:06:11 -0400

    Update winpy.py

--- Small Scripts/winpy.py
+++ Small Scripts/winpy.py
@@ -1,9 +1,11 @@
 from code import InteractiveConsole
 from os import system
-from sys import version, exit
+from sys import executable, version, exit
 
 console = InteractiveConsole()
 console.push("import os")
+python_exe = executable[:executable.rfind("\\")]
+console.push(f"os.chdir(r'{python_exe}')")
 
 print(version)
 print("Start a line with $ to be interpreted in the command line.")
